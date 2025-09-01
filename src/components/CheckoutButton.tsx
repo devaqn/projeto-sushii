3b28117862d785415beb16e70f50a29a47e57074
devaqn <pedromiguelaqn@gmail.com>
2025-09-01 15:28:14 -0300

    arrumando o cartcontext

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useCart } from '@/context/CartContext';
+import { useCart } from "../contexts/CartContext";
 
 const CheckoutButton: React.FC = () => {
   const { cartItems } = useCart();
